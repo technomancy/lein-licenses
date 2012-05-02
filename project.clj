1c29059af9db67215f425dc3d4ca928d546707d1
Phil Hagelberg <technomancy@gmail.com>
2012-05-02 16:34:28 -0700

    Release 0.1.0.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-licenses "0.1.0-SNAPSHOT"
+(defproject lein-licenses "0.1.0"
   :description "List the license of each of your dependencies."
   :url "https://github.com/technomancy/lein-licenses"
   :license {:name "Eclipse Public License"
