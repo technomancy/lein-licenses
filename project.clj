5274d27c1b1ace54d6ca6e86d9398cb2792426e2
Max Barnash <i.am@anhero.ru>
2015-02-13 16:48:22 -0800

    updated repo

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject arre/lein-licenses "0.2.0"
   :description "List the license of each of your dependencies."
-  :url "https://github.com/technomancy/lein-licenses"
+  :url "https://github.com/arre/lein-licenses"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
