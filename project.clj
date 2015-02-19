2db6f00a3bc0e5548c2c6620e52676b2cf0e0fe8
Max Barnash <i.am@anhero.ru>
2015-02-19 13:56:00 -0800

    Version 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-licenses "0.2.0-SNAPSHOT"
+(defproject lein-licenses "0.2.0"
   :description "List the license of each of your dependencies."
   :url "https://github.com/technomancy/lein-licenses"
   :license {:name "Eclipse Public License"
